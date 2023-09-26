97ce5147ddff68e17ad88018f8e4c56fa86b4f8c
Mr-Schnuyk <jekakor05@mail.ru>
2023-09-26 22:52:23 +0300

    Add glfw lib

--- build/external/glfw/cmake_install.cmake
+++ build/external/glfw/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Jenya/MyProjects/VesPet
+# Install script for directory: C:/Users/Jenya/MyProjects/VesPet/external/glfw
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,16 +34,6 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/Jenya/MyProjects/VesPet/build/external/glfw/cmake_install.cmake")
+  include("C:/Users/Jenya/MyProjects/VesPet/build/external/glfw/src/cmake_install.cmake")
 endif()
 
-if(CMAKE_INSTALL_COMPONENT)
-  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-else()
-  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
-endif()
-
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Jenya/MyProjects/VesPet/build/${CMAKE_INSTALL_MANIFEST}"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
