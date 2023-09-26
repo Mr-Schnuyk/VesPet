72587813986c59cbe552a16ae0edd5fb1f56f7cd
Mr-Schnuyk <jekakor05@mail.ru>
2023-09-26 23:28:44 +0300

    Add GLAD lib

--- build/external/glad/cmake_install.cmake
+++ build/external/glad/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Jenya/MyProjects/VesPet
+# Install script for directory: C:/Users/Jenya/MyProjects/VesPet/external/glad
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -32,23 +32,3 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("C:/Users/Jenya/MyProjects/VesPet/build/external/glfw/cmake_install.cmake")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("C:/Users/Jenya/MyProjects/VesPet/build/external/glad/cmake_install.cmake")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT)
-  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-else()
-  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
-endif()
-
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Jenya/MyProjects/VesPet/build/${CMAKE_INSTALL_MANIFEST}"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
